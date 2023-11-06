b11aec2089e76f48e4014af509f49b0cb9e1a04f
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2023-11-06 20:06:23 +0100

    php inititiation mise en forme formulaire

--- PHP/PHPInitiation/index.php
+++ PHP/PHPInitiation/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $me = [
     "prenom" => 'Jean-Jacques',
@@ -11,6 +11,7 @@ var_dump($_GET);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,23 +19,27 @@ var_dump($_GET);
     <title>PHP</title>
     <link rel="stylesheet" href="styles/index.css">
 </head>
+
 <body>
-    <a href="page2.php?prenom=<?=$me["prenom"]?>&age=<?=$me["age"]?>">Page 2</a>
+    <a href="page2.php?prenom=<?= $me["prenom"] ?>&age=<?= $me["age"] ?>">Page 2</a>
     <form action="" class="formulaire">
         <h3>Formulaire de contact</h3>
-        <div class="containerInput">
-        <label for="nom">Nom</label>
-        <input type="text" id="nom" name="nom">
-        </div>
-        <div class="containerInput">
-        <label for="prenom">Prénom</label>
-        <input type="text" id="prenom" name="prenom">
-        </div>
-        <div class="containerInput">
-        <label for="nom">Mail</label>
-        <input type="email" id="email" name="mail">
+        <div class="mainForm">
+            <div class="containerInput">
+                <label for="nom">Nom</label>
+                <input type="text" id="nom" name="nom">
+            </div>
+            <div class="containerInput">
+                <label for="prenom">Prénom</label>
+                <input type="text" id="prenom" name="prenom">
+            </div>
+            <div class="containerInput">
+                <label for="nom">Mail</label>
+                <input type="email" id="email" name="mail">
+            </div>
         </div>
         <input type="submit" class="envoyer">
     </form>
 </body>
+
 </html>
