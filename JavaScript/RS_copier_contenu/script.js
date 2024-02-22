4e44b89b163ed8d62fb7552d7fb2adbfdee543f1
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2024-02-22 13:44:32 +0100

    add animation

--- JavaScript/RS_copier_contenu/script.js
+++ JavaScript/RS_copier_contenu/script.js
@@ -1,9 +1,20 @@
 const button = document.querySelector('.card button')
 
+let runningAnimation = false
 
 button.addEventListener('click', HandleCopy)
 
 
 function HandleCopy(e){
     navigator.clipboard.writeText(e.target.previousElementSibling.textContent).then(() => alert("texte copié !"))
+    
+    if (!runningAnimation) {
+        runningAnimation = true
+        e.target.textContent = "Copié 🎉"
+     
+        setTimeout(() => {
+          e.target.textContent = "Copier 📚"
+          runningAnimation = false
+        }, 1000)
+      }
 }
