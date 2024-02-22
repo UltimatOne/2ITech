a86c23eb7e424203c2a308222024ab8ebff01770
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2024-02-22 17:52:35 +0100

    carousel

---
+++ JavaScript/RS_Slider/script.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll('.slide-img')
+const previous = document.querySelector(".previous-btn")
+const next = document.querySelector(".next-btn")
+
+previous.addEventListener("click", HandleImg)
+next.addEventListener("click", HandleImg)
+
+let i = 0;
+
+function HandleImg(e){
+    images[i].classList.remove('active')
+
+    i = i + Number(e.target.getAttribute("data-action"))
+
+    if(i<0){
+        i = images.length - 1
+    } else if(i>images.length - 1){
+        i = 0
+    }
+    images[i].classList.add("active")
+}
