c88834cf49a9c4be2c1bd070ab5fb7f8ee90f942
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2024-02-22 09:37:56 +0100

    scd commit bandiougou js

--- JavaScript/RS_Exrcices2_Cercle/script.js
+++ JavaScript/RS_Exrcices2_Cercle/script.js
@@ -1,8 +1,8 @@
 let circle = document.querySelector('.circle')
 
-document.addEventListener('mousemove',HandleCircle)
-console.dir(document);
+document.addEventListener('mousemove', HandleCircle)
+
 function HandleCircle(e){
-    circle.style.top = `${e.pageY - circle.clientHeight / 2}px`
-    circle.style.left = `${e.pageX - circle.clientWidth / 2}px`
+    circle.style.top = `${e.pageY - circle.clientWidth / 2}px`
+    circle.style.left = `${e.pageX - circle.clientHeight / 2}px`
 }
