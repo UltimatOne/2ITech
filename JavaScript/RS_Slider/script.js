34d0844ccff9289cc26e49313a93bd2dbf85c84d
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2024-02-23 09:25:48 +0100

    indentation

--- JavaScript/RS_Slider/script.js
+++ JavaScript/RS_Slider/script.js
@@ -1,20 +1,20 @@
-const images = document.querySelectorAll('.slide-img')
+const images = document.querySelectorAll(".slide-img")
 const previous = document.querySelector(".previous-btn")
 const next = document.querySelector(".next-btn")
 
 previous.addEventListener("click", HandleImg)
 next.addEventListener("click", HandleImg)
 
-let i = 0;
+let i = 0
 
-function HandleImg(e){
-    images[i].classList.remove('active')
+function HandleImg(e) {
+    images[i].classList.remove("active")
 
     i = i + Number(e.target.getAttribute("data-action"))
 
-    if(i<0){
+    if (i < 0) {
         i = images.length - 1
-    } else if(i>images.length - 1){
+    } else if (i > images.length - 1) {
         i = 0
     }
     images[i].classList.add("active")
