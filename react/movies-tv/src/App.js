33cd956358ff7559aaf4045000080103b3ec9dcd
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2025-02-13 11:46:23 +0100

    test vercel 6

--- react/movies-tv/src/App.js
+++ react/movies-tv/src/App.js
@@ -17,7 +17,7 @@ export default function App() {
     const [currentTVShow, setCurrentTVShow] = useState()
     const [videosTVShow, setVideosTVShow] = useState()
     const description = "Logo MoviesTV"
-    const siteName = "Movies TV 2"
+    const siteName = "Movies TV"
     const subtitle = "Find a show you may like"
 
     const fetchPopulars = async () => {
